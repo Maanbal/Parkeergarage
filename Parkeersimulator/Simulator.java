3f0e2b5d4b172b5427088390affc9259a5a94764
Merel Foekens <mwfoekens@gmail.com>
2019-01-21 21:47:40 +0100

    added main class

--- Parkeersimulator/Simulator.java
+++ Parkeersimulator/Simulator.java
@@ -36,10 +36,6 @@ public class Simulator {
         exitCarQueue = new CarQueue();
         simulatorView = new SimulatorView(3, 6, 30);
     }
-    
-    public static void main(String[] args) {
-		
-	}
 
     public void run() {
         for (int i = 0; i < 10000; i++) {
