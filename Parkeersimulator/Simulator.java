82697df897afd21a673c0a8996aadd0b7f289cd0
Merel Foekens <mwfoekens@gmail.com>
2019-01-21 14:06:30 +0100

    Main methode toegevoegd aan klasse Simulator

--- Parkeersimulator/Simulator.java
+++ Parkeersimulator/Simulator.java
@@ -36,6 +36,10 @@ public class Simulator {
         exitCarQueue = new CarQueue();
         simulatorView = new SimulatorView(3, 6, 30);
     }
+    
+    public static void main(String[] args) {
+		
+	}
 
     public void run() {
         for (int i = 0; i < 10000; i++) {
